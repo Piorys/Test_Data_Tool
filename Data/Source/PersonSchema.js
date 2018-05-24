85d500815f35dd62bacbbb2a18f55af44068684f
Piotr <piotrryszewski@gmail.com>
2018-05-24 22:23:14 +0200

    Created random person generator
    added methods to generate separate data as well as exporting full json of a person

---
+++ Data/Source/PersonSchema.js
@@ -0,0 +1,14 @@
+module.exports = {
+  firstName: "",
+  lastName: "",
+  mail: "",
+  username: "",
+  password: "",
+  telephone: "",
+  country: "",
+  street: "",
+  streetNumber: "",
+  flatNumber: "",
+  zipCode: "",
+  streetAddress: ""
+};
